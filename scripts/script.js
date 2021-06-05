20c422d156f012342a1a8627b7ee51b1e8e9dadf
SergeyAleev <82897736+SergeyAleev@users.noreply.github.com>
2021-06-05 21:45:06 +0300

    7

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+var popup = document.querySelector('.modal-popup');
+var buttonOpen = document.querySelector('.banner-btn-js');
+var input = popup.querySelector('input');
+var buttonClose = document.querySelector('.popup-close');
+
+buttonOpen.addEventListener('click', function() {
+ popup.classList.add('popup-open');
+ input.focus();
+})
+
+buttonClose.addEventListener('click', function() {
+ popup.classList.remove('popup-open');
+})
+
+window.addEventListener('keydown', function(event) {
+    console.log(event);
+
+    if (event.code === "Escape") {
+        if (popup.classList.contains('popup-open')) {
+            popup.classList.remove('popup-open');
+        }
+    }
+})
+
+
+
+
